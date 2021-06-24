2dc592fe97fe8baa493a85619c65485a1a2b88f8
KrishArul26 <74568334+KrishArul26@users.noreply.github.com>
2021-06-24 22:47:15 +0200

    Delete Mask_without_Image_detection directory

--- Mask_without_Image_detection/mask_detection_image.py
+++
@@ -1,8 +0,0 @@
-from mask_detector_image import mask_detection, class_selection
-
-category_index, image, boxes, scores, classes, num = mask_detection('frozen_graphs',
-                                                               '/frozen_inference_graph.pb',
-                                                               '/labelmap.pbtxt',
-                                                               2,'image8.jpg')
-
-class_selection(scores, classes, image, category_index, boxes)
