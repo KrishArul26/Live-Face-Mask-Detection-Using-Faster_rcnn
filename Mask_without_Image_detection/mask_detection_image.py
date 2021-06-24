05443fb2e53192be3d0d7517500cc8f3a5c1e855
KrishArul26 <74568334+KrishArul26@users.noreply.github.com>
2021-06-24 19:40:56 +0200

    File uploaded

--- Mask_without_Image_detection/mask_detection_image.py
+++ Mask_without_Image_detection/mask_detection_image.py
@@ -3,6 +3,6 @@ from mask_detector_image import mask_detection, class_selection
 category_index, image, boxes, scores, classes, num = mask_detection('frozen_graphs',
                                                                '/frozen_inference_graph.pb',
                                                                '/labelmap.pbtxt',
-                                                               2,'image2.jpg')
+                                                               2,'image8.jpg')
 
 class_selection(scores, classes, image, category_index, boxes)
